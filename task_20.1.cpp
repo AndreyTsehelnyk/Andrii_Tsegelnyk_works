a63ff7a6d5492239327ff6d49e31fd86bd8b84f5
AndreyTsehelnyk <98877183+AndreyTsehelnyk@users.noreply.github.com>
2022-05-31 22:38:06 +0300

    Andrii Tsehelnyk. CW 22.

---
+++ task_20.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <set>
+#include <map>
+
+int main() {
+    std::fstream in("text.txt");
+    std::string s;
+    std::map<std::string, unsigned> counter;
+
+    while (!in.eof()) {
+        in >> s;
+
+        ++counter[s];
+    }
+
+    for (const auto& [word, count] : counter)
+        std::cout << word << '\t' << count << '\n';
+
+    in.close();
+}
